e60d8b1e1d3f3549d6892829252ecaa2c34d7f27
Davidcode <dm22657@gmail.com.com>
2022-07-18 18:29:52 -0400

    karafuru

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function karafuroSlider(cambio){
+    document.querySelector('.home__imagen').src = cambio;
+}
+
+function karafuruBgChange(bg){
+    bgChange = document.querySelector('.home');
+    bgChange.style.background = bg
+}
