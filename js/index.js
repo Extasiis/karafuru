63be6ff7d0936033c2115dc340d4211a3dd5e597
Davidcode <dm22657@gmail.com.com>
2022-07-19 15:27:33 -0400

    Responsivo

--- js/index.js
+++ js/index.js
@@ -6,3 +6,12 @@ function karafuruBgChange(bg){
     bgChange = document.querySelector('.home');
     bgChange.style.background = bg
 }
+
+function karafuruToggle(){
+    const karafuruMenu = document.querySelector('.nav__navegador');
+    const karafuruLista = document.querySelector('.nav__lista');
+
+    karafuruMenu.classList.toggle('active');
+    karafuruLista.classList.toggle('active');
+
+}
