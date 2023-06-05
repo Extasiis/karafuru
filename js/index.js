138ab30ab179214dd42b9899442b0917b21d0f2d
Davidcode <dm22657@gmail.com.com>
2023-06-05 15:59:03 -0400

    page

--- js/index.js
+++ js/index.js
@@ -1,17 +1,51 @@
-function karafuroSlider(cambio){
-    document.querySelector('.home__imagen').src = cambio;
+const img = document.querySelector('.home__imagen');
+const height = img.clientHeight;
+const width = img.clientWidth;
+
+function BitcoinWhalesSlider(cambio){
+    img.src = cambio;
 }
 
-function karafuruBgChange(bg){
+img.addEventListener('mousemove', (evt) =>{
+    const {layerX, layerY} = evt;
+
+    const yRotation = (
+        (layerX - width / 2) / width
+    ) * 20
+
+    const xRotation = (
+        (layerY - height / 2) / height
+    ) * 20
+
+    const string = `
+        perspective(500px)
+        scale(1.1)
+        rotateX(${xRotation}deg)
+        rotateY(${yRotation}deg)
+        `
+        img.style.transform = string;
+})
+
+img.addEventListener('mouseout', () => {
+    const string = `
+    perspective(500px)
+    scale(1)
+    rotateX(0)
+    rotateY(0)
+    `
+    img.style.transform = string;
+})
+
+function BitcoinWhalesBgChange(bg){
     bgChange = document.querySelector('.home');
     bgChange.style.background = bg
 }
 
-function karafuruToggle(){
-    const karafuruMenu = document.querySelector('.nav__navegador');
-    const karafuruLista = document.querySelector('.nav__lista');
+function BitcoinWhalesToggle(){
+    const BitcoinWhalesMenu = document.querySelector('.nav__navegador');
+    const BitcoinWhalesLista = document.querySelector('.nav__lista');
 
-    karafuruMenu.classList.toggle('active');
-    karafuruLista.classList.toggle('active');
+    BitcoinWhalesMenu.classList.toggle('active');
+    BitcoinWhalesLista.classList.toggle('active');
 
 }
